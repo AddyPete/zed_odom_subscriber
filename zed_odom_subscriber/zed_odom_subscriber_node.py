5f4c0d352244c75191015c4fd6c301d2e932ed17
Adrian Pete Lagare <aprlagare1999@gmail.com>
2023-11-09 16:51:18 +0800

    added position subscriber

--- zed_odom_subscriber/zed_odom_subscriber_node.py
+++ zed_odom_subscriber/zed_odom_subscriber_node.py
@@ -1,6 +1,37 @@
-def main():
-    print('Hi from zed_odom_subscriber.')
+import rclpy
+from rclpy.node import Node
+
+from std_msgs.msg import String
+from nav_msgs.msg import Odometry
 
 
-if __name__ == '__main__':
+class ZedOdomSubscriber(Node):
+    def __init__(self):
+        super().__init__("zed_odom_subscriber")
+        self.subscription = self.create_subscription(
+            Odometry, "/zed2i/zed_node/odom", self.listener_callback, 10
+        )
+        self.subscription  # prevent unused variable warning
+
+    def listener_callback(self, zed_msg):
+        self.get_logger().info('X POS: "%s"' % zed_msg.pose.pose.position.x)
+        self.get_logger().info('Y POS: "%s"' % zed_msg.pose.pose.position.y)
+        self.get_logger().info('Z POS: "%s"' % zed_msg.pose.pose.position.z)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    zed_odom_subscriber = ZedOdomSubscriber()
+
+    rclpy.spin(zed_odom_subscriber)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    zed_odom_subscriber.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == "__main__":
     main()
