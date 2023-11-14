1e1c178aad412af4734f8d2df4e8afc49ace514b
Adrian Pete Lagare <aprlagare1999@gmail.com>
2023-11-14 09:49:30 +0800

    added rover action dict

--- zed_odom_subscriber/zed_odom_subscriber_node.py
+++ zed_odom_subscriber/zed_odom_subscriber_node.py
@@ -8,15 +8,26 @@ import numpy as np
 import serial
 
 # SPEED
-LINEAR_SPEED = 0.10
-ANGULAR_SPEED = 0.30
+LINEAR_SPEED = 0.25
+ANGULAR_SPEED = 0.20
 # CONSTANTS
-DIFF_ANGLE_THRESHOLD = 1
-DISTANCE_TO_TARGET_THRESHOLD = 0.3
+DIFF_ANGLE_THRESHOLD = 15  # DEGREE
+DISTANCE_TO_TARGET_THRESHOLD = 0.2  # METERS
 # ACTIONS
-LEFT_STEER = 0
-RIGHT_STEER = 2
-NO_STEER = 1
+# LEFT_STEER = 0
+# RIGHT_STEER = 2
+# NO_STEER = 1
+
+rover_actions = {
+    "Left Steer": 0,
+    "Right Steer": 1,
+    "No Steer": 2,
+    "Soft Left Steer": 3,
+    "Soft Right Steer": 4,
+}
+
+# SOFT_LEFT = 3
+# SOFT_RIGHT = 4
 
 
 class ZedOdomSubscriber(Node):
@@ -31,8 +42,9 @@ class ZedOdomSubscriber(Node):
         self.zed_twist_publisher = self.create_publisher(Twist, "/cmd_vel_zed", 10)
         self.current_waypoint = 0
         self.finished_mission = False
-        self.waypoint_objects = [[5, 0], [5, -5], [0, -5], [0, 0]]
-        # self.waypoint_objects = [[3, 0], [6, 0], [6, 6], [0, 3]]
+
+        self.waypoint_objects = [[10, 0], [10, -10], [0, -10], [0, 0]]  # METERS
+        # self.waypoint_objects = [[5, 0], [5, -5], [0, -5], [0, 0]]
 
         try:
             self.arduino = serial.Serial(
@@ -42,6 +54,10 @@ class ZedOdomSubscriber(Node):
             self.get_logger().info(f"An error occurred: {e}")
 
     def zed_odom_listener_callback(self, zed_msg):
+        self.get_logger().info(f"Finished Mission? {self.finished_mission}\n ")
+        if self.finished_mission == True:
+            self.send_serial_command(0.0, 0.0)
+            return
         x_pos = zed_msg.pose.pose.position.x
         y_pos = zed_msg.pose.pose.position.y
         z_pos = zed_msg.pose.pose.position.z
@@ -60,7 +76,7 @@ class ZedOdomSubscriber(Node):
         # self.get_logger().info(f"W ORI: {w_ori}\n")
 
         self.get_logger().info(
-            f"Current Position: X: {round(x_pos,3)} Y: {round(y_pos,3)}"
+            f"Current Position: X: {round(x_pos,3)} Y: {round(y_pos,3)}\n"
         )
 
         # rover_action = random.randint(0, 2)  # ONLY INTEGER
@@ -70,6 +86,7 @@ class ZedOdomSubscriber(Node):
         linear, angular = self.action_interpreter(rover_action)
         self.publish_cmd_vel_zed(linear, angular)
         # self.publish_position(rover_action)
+        # self.get_logger().info("====================================")
 
     def cmd_vel_all_callback(self, cmd_vel_msg):
         linear = cmd_vel_msg.linear.x
@@ -82,6 +99,10 @@ class ZedOdomSubscriber(Node):
             angular = ANGULAR_SPEED
         elif rover_action == 2:  # RIGHT
             angular = -ANGULAR_SPEED
+        elif rover_action == 3:
+            angular = ANGULAR_SPEED / 2  # SOFT LEFT
+        elif rover_action == 4:
+            angular = -ANGULAR_SPEED / 2  # SOFT RIGHT
         else:
             angular = 0.0
         return linear, angular
@@ -90,7 +111,7 @@ class ZedOdomSubscriber(Node):
     def send_serial_command(self, linear, angular, reset=0.0):
         command_string = f"{round(linear,2)},{round(-angular,2)},0,0,0,0,0,{reset}&\n"
         self.get_logger().info(
-            f"Command String {command_string}\nCommand String Length: {len(command_string)}"
+            f"Command String {command_string} | Length: {len(command_string)}"
         )
 
         try:
@@ -111,18 +132,18 @@ class ZedOdomSubscriber(Node):
 
     def action_policy(self, x, y, z, qx, qy, qz, qw):
         # assuming the orientation data in the zed odom message are already in quaternions
-        yaw = round(
-            np.degrees(
-                np.arctan2(2 * (qx * qy + qw * qz), 1 - 2 * (qy * qy + qz * qz))
-            ),
-            1,
+        yaw = np.degrees(
+            np.arctan2(2 * (qx * qy + qw * qz), 1 - 2 * (qy * qy + qz * qz))
         )
+
         target_position = self.waypoint_objects[self.current_waypoint]
 
-        self.get_logger().info(f"Current Target Waypoint: {target_position}")
-        self.get_logger().info(f"Current Waypoint #: {self.current_waypoint}\n")
-        relDisX = round(target_position[0] - x, 1)
-        relDisY = round(target_position[1] - y, 1)
+        self.get_logger().info(
+            f"Waypoint # {self.current_waypoint} | Current Target Waypoint: {target_position}\n"
+        )
+        # self.get_logger().info(f"Current Waypoint #: {self.current_waypoint}\n")
+        relDisX = target_position[0] - x
+        relDisY = target_position[1] - y
 
         if relDisX > 0 and relDisY > 0:
             theta = np.arctan(relDisY / relDisX)
@@ -141,23 +162,32 @@ class ZedOdomSubscriber(Node):
         else:
             theta = np.pi
 
-        relTheta = round(np.degrees(theta), 2)
+        relTheta = np.degrees(theta)
         diffAngle = relTheta - yaw
 
         if diffAngle <= 180:
-            diffAngle = round(diffAngle, 2)
+            diffAngle = diffAngle
         else:
             # diffAngle = round(360 - diffAngle, 2)
-            diffAngle = round(diffAngle - 360.0, 2)
+            diffAngle = diffAngle - 360.0
 
         if diffAngle >= DIFF_ANGLE_THRESHOLD:
-            action = LEFT_STEER
+            rover_action = "Left Steer"  # LEFT_STEER
         elif diffAngle < -DIFF_ANGLE_THRESHOLD:
-            action = RIGHT_STEER
-        else:
-            action = NO_STEER
+            rover_action = "Right Steer"  # RIGHT_STEER
 
-        self.get_logger().info(f"ACTION: {action}\n")
+        elif diffAngle >= DIFF_ANGLE_THRESHOLD / 2 and diffAngle < DIFF_ANGLE_THRESHOLD:
+            rover_action = "Soft Left Steer"  # SOFT_LEFT
+        elif (
+            diffAngle <= -DIFF_ANGLE_THRESHOLD / 2 and diffAngle > -DIFF_ANGLE_THRESHOLD
+        ):
+            rover_action = "Soft Right Steer"  # SOFT_RIGHT
+        else:
+            rover_action = "No Steer"  # NO_STEER
+
+        action_code = rover_actions[rover_action]
+
+        self.get_logger().info(f"ACTION: {action_code} | {rover_action}\n")
         self.get_logger().info(f"DIFF ANGLE: {diffAngle}\n")
 
         distance_to_target = np.sqrt(
@@ -176,11 +206,11 @@ class ZedOdomSubscriber(Node):
         ):
             self.current_waypoint += 1
 
-        if self.finished_mission == True:
-            self.current_waypoint = 0
-            self.finished_mission = False
+        # if self.finished_mission == True:
+        #     self.current_waypoint = 0
+        #     self.finished_mission = False
 
-        return action
+        return action_code
 
 
 def main(args=None):
